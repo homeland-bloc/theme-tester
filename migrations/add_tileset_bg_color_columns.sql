e2f542c797d5d63b49ea44a2c05a8905a4a1f3c2
homeland-bloc <mimikriyawow@gmail.com>
2026-05-15 23:52:00 +0100

    Delete migrations directory

--- migrations/add_tileset_bg_color_columns.sql
+++
@@ -1,7 +0,0 @@
--- Fix 3C: Add nullable background colour columns to tilesets table
--- Run this in the Supabase SQL editor or via psql.
--- These columns are backfilled manually after running the Extract Atlas BG Colours tool.
-
-ALTER TABLE tilesets
-  ADD COLUMN IF NOT EXISTS bg_color_light text,
-  ADD COLUMN IF NOT EXISTS bg_color_dark  text;
